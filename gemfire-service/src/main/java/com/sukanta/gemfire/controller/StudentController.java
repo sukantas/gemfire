d026eaee32fae41f68e9db885570c308ef53f282
sukantas <sukantasamanta2003@gmail.com>
2017-04-03 13:53:58 +0530

    added response

--- gemfire-service/src/main/java/com/sukanta/gemfire/controller/StudentController.java
+++ gemfire-service/src/main/java/com/sukanta/gemfire/controller/StudentController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sukanta.gemfire.models.Student;
@@ -19,6 +20,7 @@ public class StudentController {
 	CacheService cache;
 	
 	@RequestMapping(method = RequestMethod.GET, path="/stored")
+	@ResponseBody
 	public String storeToGemfire(){
 		
 		Student s = new Student();
