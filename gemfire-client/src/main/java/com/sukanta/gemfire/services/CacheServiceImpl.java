1e81f762d405e4491255ef16a6414a28ac910867
sukantas <sukantasamanta2003@gmail.com>
2017-04-03 16:43:33 +0530

    push

--- gemfire-client/src/main/java/com/sukanta/gemfire/services/CacheServiceImpl.java
+++ gemfire-client/src/main/java/com/sukanta/gemfire/services/CacheServiceImpl.java
@@ -1,10 +1,12 @@
 package com.sukanta.gemfire.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.sukanta.gemfire.models.Student;
 import com.sukanta.gemfire.repositories.StudentRepository;
 
+@Service
 public class CacheServiceImpl  implements CacheService{
 
 	@Autowired
